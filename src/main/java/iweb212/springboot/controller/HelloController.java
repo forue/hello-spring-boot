3eb76754efc1662ac289521ed83bc95a74c434d7
发友 <1540579492@qq.com>
2019-08-19 15:37:40 +0800

    ok

--- src/main/java/iweb212/springboot/controller/HelloController.java
+++ src/main/java/iweb212/springboot/controller/HelloController.java
@@ -7,14 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/")
     public String home(){
         return "这是由SpringBoot生成的";
     }
-//    @GetMapping("/json")
-//    public User user(){
-//        return new User(
-//                "张三",1l,
-//                new Address("苏园路","玄武区","南京市"));
-//    }
+
+    @GetMapping("/json")
+    public User user(){
+        return new User(
+                "张三",1l,
+                new Address("长安街266号","丰台区","北京市"));
+    }
 }
